0e8a6f57e185d2f1db270701f76331cb57f66159
Vesamet <slab95@pm.me>
2019-11-07 22:25:27 -0500

    add Vuetify library

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import i18n from './i18n'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   i18n,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
